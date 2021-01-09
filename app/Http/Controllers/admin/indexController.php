1eaa386fe442f80a810516a718df19e75f52255e
halukbuyukozkan <halukbuyukozkan@gmail.com>
2021-01-09 05:23:14 +0300

    Admin panel created

--- app/Http/Controllers/admin/indexController.php
+++ app/Http/Controllers/admin/indexController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class indexController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.index');
+    }
 }
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,18 +3,18 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('admin_css/assets/img/apple-icon.png')}}" />
+    <link rel="icon" type="image/png" href="{{asset('admin_css/assets/img/favicon.png')}}" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>Material Dashboard by Creative Tim</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
     <!-- Bootstrap core CSS     -->
-    <link href="{{asset('admin/assets/css/bootstrap.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('admin_css/assets/css/bootstrap.min.css')}}" rel="stylesheet" />
     <!--  Material Dashboard CSS    -->
-    <link href="{{asset('assets/css/material-dashboard.css?v=1.2.0')}}" rel="stylesheet" />
+    <link href="{{asset('admin_css/assets/css/material-dashboard.css?v=1.2.0')}}" rel="stylesheet" />
     <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="{{asset('assets/css/demo.css" rel="stylesheet')}}" />
+    <link href="{{asset('admin_css/assets/css/demo.css" rel="stylesheet')}}" />
     <!--     Fonts and icons     -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons" rel='stylesheet'>
@@ -129,23 +129,23 @@
         </div>
     </div>
     <!--   Core JS Files   -->
-    <script src="../assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-    <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="../assets/js/material.min.js" type="text/javascript"></script>
+    <script src="{{asset('admin_css/assets/js/jquery-3.2.1.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('admin_css/assets/js/bootstrap.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('admin_css/assets/js/material.min.js')}}" type="text/javascript"></script>
     <!--  Charts Plugin -->
-    <script src="../assets/js/chartist.min.js"></script>
+    <script src="{{asset('admin_css/assets/js/chartist.min.js')}}"></script>
     <!--  Dynamic Elements plugin -->
-    <script src="../assets/js/arrive.min.js"></script>
+    <script src="{{asset('admin_css/assets/js/arrive.min.js')}}"></script>
     <!--  PerfectScrollbar Library -->
-    <script src="../assets/js/perfect-scrollbar.jquery.min.js"></script>
+    <script src="{{asset('admin_css/assets/js/perfect-scrollbar.jquery.min.js')}}"></script>
     <!--  Notifications Plugin    -->
-    <script src="../assets/js/bootstrap-notify.js"></script>
+    <script src="{{asset('admin_css/assets/js/bootstrap-notify.js')}}"></script>
     <!--  Google Maps Plugin    -->
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
     <!-- Material Dashboard javascript methods -->
-    <script src="../assets/js/material-dashboard.js?v=1.2.0"></script>
+    <script src="{{asset('admin_css/assets/js/material-dashboard.js?v=1.2.0')}}"></script>
     <!-- Material Dashboard DEMO methods, don't include it in your project! -->
-    <script src="../assets/js/demo.js"></script>
+    <script src="{{asset('admin_css/assets/js/demo.js')}}"></script>
     <script type="text/javascript">
         $(document).ready(function() {
 
