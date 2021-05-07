55a6be8f5b2304d7425b3342ff466e6f8b939a02
halukbuyukozkan <halukbuyukozkan@gmail.com>
2021-05-07 13:44:19 +0300

    Show orders in admin panel

--- routes/web.php
+++ routes/web.php
@@ -70,6 +70,13 @@ Route::group(['namespace' => 'admin', 'prefix' => 'admin', 'as' => 'admin.'], fu
         Route::post('/edit/{id}', 'indexController@update')->name('edit.post');
         Route::get('/delete/{id}', 'indexController@delete')->name('delete');
     });
+    Route::group(['namespace' => 'order', 'prefix' => 'order', 'as' => 'order.'], function () {
+        Route::get('/', 'indexController@index')->name('index');
+        Route::get('/create', 'indexController@create')->name('create');
+        Route::post('/create', 'indexController@store')->name('create.post');
+        Route::get('/detail/{id}', 'indexController@detail')->name('detail');
+        Route::get('/delete/{id}', 'indexController@delete')->name('delete');
+    });
 });
 
 Auth::routes();
